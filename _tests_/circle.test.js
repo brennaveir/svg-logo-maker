84687ab30379ef046bdadca22d133472a86d72ce
brennaveir <brennaveir@hotmail.com>
2023-02-07 14:05:33 -0500

    removed redundancy in tests that was testing for shape color twice

--- _tests_/circle.test.js
+++ _tests_/circle.test.js
@@ -1,37 +1,32 @@
-const Circle = require("../lib/circle") 
+const Circle = require("../lib/circle")
 
 describe('Circle', () => {
-  
+
     describe('color', () => {
-      it('should render color when render method is called', () => {
-        const shape = new Circle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
-  
-      });
-      it('should return characters', () => {
-          const character = "ABK"
-          const shape = new Circle(character);
-          expect(shape.characters).toEqual(character);
-    
+
+        it('should return characters', () => {
+            const character = "ABK"
+            const shape = new Circle(character);
+            expect(shape.characters).toEqual(character);
+
         });
         it('should return text color', () => {
-          const textColor = "purple"
-          const shape = new Circle("BKV", textColor);
-          expect(shape.textColor).toEqual(textColor);
-    
+            const textColor = "purple"
+            const shape = new Circle("BKV", textColor);
+            expect(shape.textColor).toEqual(textColor);
+
         });
-        it('should return shape color', () => {
-          const color = "red"
-          const shape = new Circle("BKV", "blue", color);
-          expect(shape.color).toEqual(color);
-    
+        it('should render color when render method is called', () => {
+            const shape = new Circle();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
+
         });
         it('should return formation', () => {
-          const formation = "circle"
-          const shape = new Circle("BKV", "blue", "pink", formation);
-          expect(shape.formation).toEqual(formation);
-    
+            const formation = "circle"
+            const shape = new Circle("BKV", "blue", "pink", formation);
+            expect(shape.formation).toEqual(formation);
+
         });
     });
-  });
+});
--- _tests_/square.test.js
+++ _tests_/square.test.js
@@ -1,37 +1,32 @@
-const Square = require("../lib/square") 
+const Square = require("../lib/square")
 
 describe('Square', () => {
-  
+
     describe('color', () => {
-      it('should render color when render method is called', () => {
-        const shape = new Square();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<rect x="40" y="40" width="200" height="200" fill="blue" />');
-  
-      });
-      it('should return characters', () => {
-          const character = "ABK"
-          const shape = new Square(character);
-          expect(shape.characters).toEqual(character);
-    
+
+        it('should return characters', () => {
+            const character = "ABK"
+            const shape = new Square(character);
+            expect(shape.characters).toEqual(character);
+
         });
         it('should return text color', () => {
-          const textColor = "purple"
-          const shape = new Square("BKV", textColor);
-          expect(shape.textColor).toEqual(textColor);
-    
+            const textColor = "purple"
+            const shape = new Square("BKV", textColor);
+            expect(shape.textColor).toEqual(textColor);
+
         });
-        it('should return shape color', () => {
-          const color = "red"
-          const shape = new Square("BKV", "blue", color);
-          expect(shape.color).toEqual(color);
-    
+        it('should render color when render method is called', () => {
+            const shape = new Square();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<rect x="40" y="40" width="200" height="200" fill="blue" />');
+
         });
         it('should return formation', () => {
-          const formation = "square"
-          const shape = new Square("BKV", "blue", "pink", formation);
-          expect(shape.formation).toEqual(formation);
-    
+            const formation = "square"
+            const shape = new Square("BKV", "blue", "pink", formation);
+            expect(shape.formation).toEqual(formation);
+
         });
     });
-  });
+});
